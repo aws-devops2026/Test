90e7b3de6b3bb41caa41e856321d5a996699f821
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-17 13:20:35 +0530

    all-aws

---
+++ specialvariable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# $0 is to print the script name.
+
+firstVar=$1
+secondVar=$2
+
+echo "I  am printing the first argument $firstVar"
+echo "I  am printing the second argument $secondVar"
