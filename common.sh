5fd7d4e139ca9e6886d025c90f7ca871cd6864d2
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-17 22:38:17 +0530

    all-aws

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 common_fun() {
-    echo "printing the load average $(uptime)"
-    echo "Name of the script is $0"
+    echo -e "printing the load average \e[35m $(uptime) \e[0m"
+    echo -e "Name of the script is \e[35m $0 \e[0m"
 }
