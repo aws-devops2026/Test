6d6338555cdca23d46a2698e5d68f2d3eee561af
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-17 16:15:58 +0530

    all-aws

--- specialvariable.sh
+++ specialvariable.sh
@@ -10,3 +10,6 @@ echo "I  am printing the first argument $firstVar"
 echo "I  am printing the second argument $secondVar"
 
 echo "Name of executed script is $0"
+
+echo "argument used in the script are $#"
+echo "argument used in the script are $@"
