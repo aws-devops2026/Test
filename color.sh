e22ce271472b9f340da740320f86df3c17076312
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-17 10:34:45 +0530

    all-aws

--- color.sh
+++ color.sh
@@ -6,3 +6,6 @@ echo -e "\e[32m this is green color \e[0m"
 echo -e "\e[33m this is yellow color \e[0m"
 echo -e "\e[34m this is blue color \e[0m"
 echo -e "\e[35m this is magenta color \e[0m"
+
+#print the background clour
+echo -e "\e[43;31m printing on Yellow \[0m"
