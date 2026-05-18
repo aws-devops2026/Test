a4fc5bf2c8d756ab85fbd1441f2b4bc925dc2823
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-18 09:22:35 +0530

    all-aws

--- redirection.sh
+++ redirection.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 # cat /etc/passwd | wc -l  --
+
+# Redirectors
+# How to take input, how to pass the output, how to pass the errors ?
+
+# Redirectors are of 2 types in bash: 
+# 1. Input Redirector       ( < ) - we use this to take the input from a file :  mysql < /tmp/robot.sql ) 
+# 2. Output Redirector
+
+# Outputs are of 3 types:
+    # 1) Standard Output            1> or >  or >> ( Appends the existing content, if not it overrides )
+    # 2) Standard Error             2> or 2>>
+    # 3) Standard Output & Error    &> or $>>
