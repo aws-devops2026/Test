4855734ac32bb163415e07359076be5a037d1cdd
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-18 09:06:00 +0530

    all-aws

--- shebang.sh
+++ shebang.sh
@@ -1,5 +1,15 @@
 #!/bin/bash 
 
+# First line should always start with shebang notation!
+# Shebang notation always starts with "#!"
+# In linux, apart from Shebang notation, anything that starts with # is a comment, that means that's just for human understanding purpose only
+
+# How do I execute this script ?
+# bash scriptName
+
+# $0 is a special chacacter to print the name of the script
+
+#...................
 
 echo "Hello world"
 sleep 3
