687e2600ff012b19dcc5bd3f564d18f49e955594
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-18 09:20:02 +0530

    all-aws

--- read.sh
+++ read.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+echo "This script will take the input from the user by interrupting the execution"
+
+read -p "Name of the user executing the script:" NAME
+
+echo "User who executed the script is $NAME"
+
+echo "Above is the user who executed the script"
+echo "Name of the script is $0"
+
+
+#........................
 echo "This script will take input from the user by interupting the execution"
 read -p "Name of the user executed the script is $NAME"
 echo "Above is the user who executed the script"
