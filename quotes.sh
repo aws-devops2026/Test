81e66387cb9e82675e582cc6973241ce29838037
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-18 09:18:59 +0530

    all-aws

--- quotes.sh
+++ quotes.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# ""   : Double Quotes 
+# ''   : Single Quotes   : # Single Quotes Always Eliminates The Power Of The Special Variable 
+
+a=10
+echo "Printing the value of a ${a}"
+echo 'Printing the value of a ${a}'
+echo 'printing the exit code of the previous command $?'
+
+
+
+#.............................
 # "" Double quotes
 # '' single quotes
 # single quotes always eleminates the power of the special values.
