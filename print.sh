1f23d7c5016b0a168d2564a43609ce533b7d68c7
vijayt <vijaykumar.thunugur@gmail.com>
2026-05-18 09:08:19 +0530

    all-aws

--- print.sh
+++ print.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+# echo hai 
+# echo hello
+# echo welcome
+# echo devsecops
+
+# Special Characters 
+# \n: Moves the cursor to the next line
+# \t: Moves the cursor to the next tab
+
+echo -e "hai \n hello \n welcome \n devsecops"
+
+echo -e "\t\t Hai \n Welcome"
+
 echo "hai \n hello \n welcome \ndevops"
 echo -e "\e[36m this is cyan color \e[0m"
